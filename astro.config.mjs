00ee1050041f7cf978acfd4f58b2911d557eb698
Brett Tanner <bretttanner171@gmail.com>
2024-02-24 00:08:43 +0900

    Create something resembling a landing page

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,45 +4,41 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	image: {
-		service: {
-			entrypoint: "astro/assets/services/noop",
-		},
-	},
-	integrations: [
-		starlight({
-			title: "Brett's Notes",
-			social: {
-				github: "https://github.com/Brett-Tanner/wiki",
-			},
-			sidebar: [
-				{
-					label: "CSS",
-					autogenerate: { directory: "css" },
-				},
-				{
-					label: "git",
-					autogenerate: { directory: "git" },
-				},
-				{
-					label: "HTML",
-					autogenerate: { directory: "html" },
-				},
-				{
-					label: "Javascript",
-					autogenerate: { directory: "javascript" },
-				},
-				{
-					label: "Python",
-					autogenerate: { directory: "python" },
-				},
-				{
-					label: "Rails",
-					autogenerate: { directory: "rails" },
-				},
-			],
-			customCss: ["./src/tailwind.css"],
-		}),
-		tailwind({ applyBaseStyles: false }),
-	],
+  image: {
+    service: {
+      entrypoint: "astro/assets/services/noop",
+    },
+  },
+  integrations: [
+    starlight({
+      title: "Brett's Notes",
+      social: {
+        github: "https://github.com/Brett-Tanner/wiki",
+      },
+      sidebar: [
+        {
+          label: "CSS",
+          autogenerate: { directory: "css" },
+        },
+        {
+          label: "HTML",
+          autogenerate: { directory: "html" },
+        },
+        {
+          label: "Javascript",
+          autogenerate: { directory: "javascript" },
+        },
+        {
+          label: "Python",
+          autogenerate: { directory: "python" },
+        },
+        {
+          label: "Rails",
+          autogenerate: { directory: "rails" },
+        },
+      ],
+      customCss: ["./src/tailwind.css"],
+    }),
+    tailwind({ applyBaseStyles: false }),
+  ],
 });
