b3e8d614cc5fef4d0d6f1334177593c71a41f9d7
Brett <bretttanner171@gmail.com>
2024-03-22 18:40:53 +0900

    Add articles & books sections

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,41 +4,49 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  image: {
-    service: {
-      entrypoint: "astro/assets/services/noop",
-    },
-  },
-  integrations: [
-    starlight({
-      title: "Brett's Notes",
-      social: {
-        github: "https://github.com/Brett-Tanner/wiki",
-      },
-      sidebar: [
-        {
-          label: "CSS",
-          autogenerate: { directory: "css" },
-        },
-        {
-          label: "HTML",
-          autogenerate: { directory: "html" },
-        },
-        {
-          label: "Javascript",
-          autogenerate: { directory: "javascript" },
-        },
-        {
-          label: "Python",
-          autogenerate: { directory: "python" },
-        },
-        {
-          label: "Rails",
-          autogenerate: { directory: "rails" },
-        },
-      ],
-      customCss: ["./src/tailwind.css"],
-    }),
-    tailwind({ applyBaseStyles: false }),
-  ],
+	image: {
+		service: {
+			entrypoint: "astro/assets/services/noop",
+		},
+	},
+	integrations: [
+		starlight({
+			title: "Brett's Notes",
+			social: {
+				github: "https://github.com/Brett-Tanner/wiki",
+			},
+			sidebar: [
+				{
+					label: "Articles",
+					autogenerate: { directory: "articles" },
+				},
+				{
+					label: "Books",
+					autogenerate: { directory: "books" },
+				},
+				{
+					label: "CSS",
+					autogenerate: { directory: "css" },
+				},
+				{
+					label: "HTML",
+					autogenerate: { directory: "html" },
+				},
+				{
+					label: "Javascript",
+					autogenerate: { directory: "javascript" },
+				},
+				{
+					label: "Python",
+					autogenerate: { directory: "python" },
+				},
+				{
+					label: "Rails",
+					autogenerate: { directory: "rails" },
+				},
+			],
+			customCss: ["./src/tailwind.css"],
+		}),
+		tailwind({ applyBaseStyles: false }),
+	],
 });
