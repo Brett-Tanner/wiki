e5d03e0b546f545733c063ef7ef07a56b4db7878
Brett Tanner <bretttanner171@gmail.com>
2024-09-24 20:10:09 +0900

    Add obsidian as submodule & autogenerate

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,39 +18,58 @@ export default defineConfig({
 			sidebar: [
 				{
 					label: "Articles",
-					autogenerate: { directory: "articles" },
+					autogenerate: { directory: "obsidian/Articles" },
+					collapsed: true,
 				},
 				{
 					label: "Books",
-					autogenerate: { directory: "obsidian/books" },
+					autogenerate: { directory: "obsidian/Books" },
+					collapsed: true,
 				},
 				{
 					label: "CSS",
 					autogenerate: { directory: "css" },
+					collapsed: true,
 				},
 				{
 					label: "Go",
 					autogenerate: { directory: "go" },
+					collapsed: true,
 				},
 				{
 					label: "HTML",
 					autogenerate: { directory: "html" },
+					collapsed: true,
 				},
 				{
 					label: "Javascript",
 					autogenerate: { directory: "javascript" },
+					collapsed: true,
+				},
+				{
+					label: "Leetcode",
+					autogenerate: { directory: "obsidian/Leetcode" },
+					collapsed: true,
 				},
 				{
 					label: "Python",
 					autogenerate: { directory: "python" },
+					collapsed: true,
 				},
 				{
 					label: "Rails",
 					autogenerate: { directory: "rails" },
+					collapsed: true,
 				},
 				{
 					label: "Rust",
 					autogenerate: { directory: "rust" },
+					collapsed: true,
+				},
+				{
+					label: "Tools",
+					autogenerate: { directory: "obsidian/Tools" },
+					collapsed: true,
 				},
 			],
 			customCss: ["./src/tailwind.css"],
--- src/content/docs/articles/blog_posts.md
+++
@@ -1,9 +0,0 @@
----
-title: Blog Posts
-description: Links to blog posts I found interesting with one line reasons
-edit_url: false
----
-
-## [JS Optimisation](https://romgrk.com/posts/optimizing-javascript)
-
-Lots of good strategies, and a grounded approach to what to optimise/whether you need to.
